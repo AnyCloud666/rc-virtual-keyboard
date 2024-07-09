86fb4438c7fa517cf85ea792303f8ed3fe5cf1d5
cloud <yunliu@wwitechs.com>
2024-07-09 14:54:52 +0800

    feat: 数字键新增styles属性

--- src/NumberKeyboard/index.tsx
+++ src/NumberKeyboard/index.tsx
@@ -6,9 +6,14 @@ import './style.css';
 /** 数字键盘 */
 const NumberKeyboard = ({
   style,
+  styles,
   onClick,
 }: {
   style?: CSSProperties;
+  styles?: {
+    /** 单个按键样式 */
+    numberKeyItem?: CSSProperties;
+  };
   onClick?: (e: VKB.KeyboardAttributeType) => void;
 }) => {
   return (
@@ -16,6 +21,7 @@ const NumberKeyboard = ({
       {numberKeys.map((item) => {
         return (
           <div
+            style={styles?.numberKeyItem}
             className="number-key-item"
             key={item.keyCode}
             onClick={() => onClick && onClick(item)}
