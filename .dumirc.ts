1a25541c7623dd833132bd5a5f027dcfb92dd9b2
cloud <57932069+AnyCloud666@users.noreply.github.com>
2025-01-03 16:21:13 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'react-vitrual-keyboard';
+
 export default defineConfig({
   outputPath: 'docs-dist',
   logo: '/logo.png',
-  base: '/',
-  publicPath: '/',
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   themeConfig: {
     name: 'react-virtual-keyboard',
   },
